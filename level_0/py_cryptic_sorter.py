22518da1b9417ed4df5565602c461fc900ff4f65
Adriana Neves <158789061+adriananevex@users.noreply.github.com>
2026-06-26 18:13:42 +0000

    doing

--- level_0/py_cryptic_sorter.py
+++ level_0/py_cryptic_sorter.py
@@ -7,3 +7,9 @@ def cryptic_sorter(t_list):
     return counter
   final_list = sorted(t_list, key=lambda s: (len(s), s.lower(), vowels_counter(s)))
   return final_list
+
+print(cryptic_sorter(["apple","cat","banana","dog","elephant"]))
+print(cryptic_sorter(["aaa","bbb","AAA","BBB"]))
+print(cryptic_sorter(["hello","world","hi","test"]))
+print(cryptic_sorter([]))
+print(cryptic_sorter([""]))
