166d9fc7e38bd496a3c577e67296b675fe0f6b8b
Adriana Neves <158789061+adriananevex@users.noreply.github.com>
2026-06-26 18:11:56 +0000

    doing

--- level_0/py_cryptic_sorter.py
+++ level_0/py_cryptic_sorter.py
@@ -3,7 +3,7 @@ def cryptic_sorter(t_list):
     counter = 0
     for i in s.lower():
       if i in "aeiou":
-        counter *= 1
+        counter += 1
     return counter
   final_list = sorted(t_list, key=lambda s: (len(s), s.lower(), vowels_counter(s)))
   return final_list
