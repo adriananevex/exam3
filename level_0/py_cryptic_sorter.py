218d8fc9d8f2fb42102a10141f7361d3f856067f
Adriana Neves <158789061+adriananevex@users.noreply.github.com>
2026-06-26 18:43:16 +0000

    doing

--- level_0/py_cryptic_sorter.py
+++ level_0/py_cryptic_sorter.py
@@ -5,7 +5,7 @@ def cryptic_sorter(t_list):
       if i in "aeiou":
         counter += 1
     return counter
-  final_list = sorted(t_list, key=lambda s: (len(s), s.lower(), vowels_counter(s)))
+  final_list = sorted(t_list, key=lambda s: (len(s), s.lower(), vowels_counter(s), s))
   return final_list
 
 print(cryptic_sorter(["apple","cat","banana","dog","elephant"]))
